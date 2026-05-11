ff428724497cba406dd9884a8068d1455bb47e1a
Khushi <khushithisside26@gmail.com>
2026-05-11 12:26:36 +0530

    Add avatar upload with multer and Cloudinary

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const authMiddleware = require("../middlewares/authMiddleware");
 const User = require("../models/User");
+const upload = require("../middlewares/uploadMiddleware");
+const cloudinary = require("../config/cloudinary");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
 router.get("/me", authMiddleware, async function(req, res){
     return res.status(200).json({
@@ -34,6 +37,45 @@ router.put("/update-profile", authMiddleware, async function(req, res){
             message: "Something went wrong"
         })
     }
+})
+
+
+router.post("/upload-avatar", authMiddleware, async function(req, res){
+    try{
+        const result = await cloudinary.uploader.upload(req.file.path);
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user.id,
+            {
+                avatar: result.secure_url
+            },
+            {
+                new: true
+            }
+        )
+        
+        return res.status(200).json({
+            message: "Avatar uploaded successfully",
+            avatar: updatedUser.avatar
+        })
+    } catch(err){
+        return res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+    
+})
+
+
+router.get("/all-users", authMiddleware, adminMiddleware, async function(req, res){
+    try{
+        const users = await User.find();
+        return res.status(200).json({
+            users
+        })
+    } catch(err){
+        message: "Something went wrong"
+    }
+    
 })
 
 
