55ae318aeee99663590d3b905991d1316e17905a
Khushi <khushithisside26@gmail.com>
2026-05-11 13:02:42 +0530

    Add post relationships and populate queries

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const Post = require("../models/Post");
 
+
 router.post("/create", authMiddleware, async function(req, res){
     try{
         const {title, content} = req.body;
@@ -25,9 +26,10 @@ router.post("/create", authMiddleware, async function(req, res){
     }
 })
 
+
 router.get("/all", async function(req, res){
     try{
-        const posts = await Post.find().populate("user");
+        const posts = await Post.find().populate("user", "name email avatar");
         return res.status(200).json({
             posts
         })
@@ -38,6 +40,23 @@ router.get("/all", async function(req, res){
     }
     
 })
+
+router.get("/my-posts", authMiddleware, async function(req, res){
+    try{
+        const posts = await Post.find({
+            user: req.user.id
+        });
+        return res.status(200).json([
+            posts
+        ])
+    } catch(err){
+        return res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+    
+})
+
 
 
 module.exports = router;
